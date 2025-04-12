10e677884209ee0e8e5f47cb97a80a38c10cdb9d
dev-Baran1155 <baranc1155@gmail.com>
2025-04-12 18:30:51 +0300

    odd even finder
    Finds the odd pair of the number you wrote at the bottom of the code (38)

---
+++ tek-cift.py
@@ -0,0 +1,11 @@
+def tek_mi_çift_mi(sayı): #burda fonksiyon işlevini kullandık def ile atadık
+
+    #bu if kısmında ise koşulları kullandık
+    
+    if sayı % 2 == 0:
+        print(f"{sayı} sayısı çift bir sayıdır") 
+
+    else:
+        print(f"{sayı} tek bir sayıdır")
+
+tek_mi_çift_mi(38) # burda ise ("bu kısıma giridiniz sayı") buraya bağlı olarak tek mi çift mi oldugunu anlıyor
